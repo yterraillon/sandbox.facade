a12b656aca1aa22bdb59a70aad04adb6939f2a28
yterraillon <yterraillon@gmail.com>
2022-03-06 14:28:24 +0100

    Basic API Setup

---
+++ Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Authentication.Commands.AuthenticateWithSpotify;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthenticationController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> AuthenticateWithSpotify()
+    {
+        var response = await _mediator.Send(new AuthenticateWithSpotify.Request());
+        return Ok(response);
+    }
+}
---
+++ Application/Authentication/Commands/AuthenticateWithSpotify/AuthenticateWithSpotify.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Application.Authentication.Commands.AuthenticateWithSpotify;
+
+public static class AuthenticateWithSpotify
+{
+    public record Response(string Message);
+
+    public record Request : IRequest<Response>;
+    
+    public class Handler : IRequestHandler<Request, Response>
+    {
+        public Handler()
+        {
+            
+        }
+        
+        public Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new Response("OK"));
+        }
+    }
+}
